6991386ce83ecb709a9f9b2405ad571333b9ef5c
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-04-20 14:42:41 +0300

    add sot type methods

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,19 +1,55 @@
 import AbstractComponent from "./AbstractComponent";
 
-const createSortTemplate = () => {
+const SortType = {
+  DEFAULT: `default`,
+  SORT_DATE: `sort-date`,
+  SORT_RATING: `sort-rating`
+};
+
+const createSortTemplate = (currentSortType) => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort="${SortType.DEFAULT}" class="sort__button ${currentSortType === SortType.DEFAULT ? `sort__button--active` : ``}">Sort by default</a></li>
+      <li><a href="#" data-sort="${SortType.SORT_DATE}" class="sort__button ${currentSortType === SortType.SORT_DATE ? `sort__button--active` : ``}">Sort by date</a></li>
+      <li><a href="#" data-sort="${SortType.SORT_RATING}" class="sort__button" ${currentSortType === SortType.SORT_RATING ? `sort__button--active` : ``}>Sort by rating</a></li>
     </ul>`
   );
 };
 
 class Sort extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._currentSortType);
+  }
+
+  getSortType() {
+    return this._currentSortType;
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      const target = evt.target;
+
+      if (target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = target.dataset.sort;
+
+      if (sortType === this._currentSortType) {
+        return;
+      }
+
+      this._currentSortType = sortType;
+      handler(this._currentSortType);
+    });
   }
 }
 
+export {SortType};
 export default Sort;
--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,3 +1,4 @@
+import {ExtraMovieTitle, MOVIE_SHOW_START, SHOWING_MOVIE_COUNT_BY_BUTTON} from "../const";
 import {remove, render, RenderPosition} from "../utils/render";
 import MovieComponent from "../components/movie";
 import MoviePopupComponent from "../components/movie-popup";
@@ -5,9 +6,14 @@ import FilmExtraComponent from "../components/film-extra";
 import MovieContainerComponent from "../components/movie-container";
 import NoDataComponent from "../components/no-data";
 import MovieListComponent from "../components/movie-list";
-import {ExtraMovieTitle, MOVIE_SHOW_START, SHOWING_MOVIE_COUNT_BY_BUTTON} from "../const";
 import ShowMoreButtonComponent from "../components/show-more-button";
 import {getMostCommentedFilms, getTopRatingMovies} from "../utils/common";
+import MenuComponent from "../components/menu";
+import SortComponent, {SortType} from "../components/sorting";
+import MovieMainComponent from "../components/movie-main";
+import {getMenu} from "../mock/filter";
+
+const menu = getMenu();
 
 const renderMovie = (movieListElement, movie) => {
   const bodyElement = document.querySelector(`body`);
@@ -66,6 +72,35 @@ const renderExtraMovie = (movieMainComponent, movies, title) => {
   });
 };
 
+const renderMovies = (movieListElement, movies) => {
+  movies.forEach((movie) => {
+    renderMovie(movieListElement, movie);
+  });
+};
+
+const getSortedMovies = (sortType, movies, from, to) => {
+  let sortedMovies = [];
+  const showingMovies = movies.slice();
+
+  switch (sortType) {
+    case SortType.SORT_DATE:
+      sortedMovies = showingMovies.sort((a, b) => {
+        return b.release.date - a.release.date;
+      });
+      break;
+    case SortType.SORT_RATING:
+      sortedMovies = showingMovies.sort((a, b) => {
+        return b.rating - a.rating;
+      });
+      break;
+    case SortType.DEFAULT:
+      sortedMovies = showingMovies;
+      break;
+  }
+
+  return sortedMovies.slice(from, to);
+};
+
 class PageController {
   constructor(container) {
     this._container = container;
@@ -74,52 +109,78 @@ class PageController {
     this._showMoreButtonComponent = new ShowMoreButtonComponent();
     this._movieListComponent = new MovieListComponent();
     this._noDataComponent = new NoDataComponent();
+    this._menuComponent = new MenuComponent(menu);
+    this._sortComponent = new SortComponent();
+    this._movieMainComponent = new MovieMainComponent();
   }
 
   render(movies) {
-    const container = this._container.getElement();
+    const renderShowMoreButton = () => {
+      if (showMovieCount >= movies.length) {
+        return;
+      }
+
+      render(movieListElement, this._showMoreButtonComponent, RenderPosition.BEFORE_END);
+
+      this._showMoreButtonComponent.setClickHandler(() => {
+        console.log(`was click`);
+        const prevMovieShowCount = showMovieCount;
+        showMovieCount = showMovieCount + SHOWING_MOVIE_COUNT_BY_BUTTON;
+        const sortedMovies = getSortedMovies(this._sortComponent.getSortType(), movies, prevMovieShowCount, showMovieCount);
+
+        renderMovies(movieListContainerElement, sortedMovies);
+
+        if (showMovieCount >= movies.length) {
+          remove(this._showMoreButtonComponent);
+        }
+      });
+    };
+
+    const container = this._container;
+
+    render(container, this._menuComponent, RenderPosition.BEFORE_END);
+    render(container, this._sortComponent, RenderPosition.BEFORE_END);
+    render(container, this._movieMainComponent, RenderPosition.BEFORE_END);
+
+    const filmsElement = this._movieMainComponent.getElement();
 
     if (movies.length === 0) {
-      render(container, this._noDataComponent, RenderPosition.BEFORE_END);
+      render(filmsElement, this._noDataComponent, RenderPosition.BEFORE_END);
       return;
     }
 
-    render(container, this._movieListComponent, RenderPosition.BEFORE_END);
+    render(filmsElement, this._movieListComponent, RenderPosition.BEFORE_END);
 
     const movieListElement = this._movieListComponent.getElement();
     render(movieListElement, this._movieContainerComponent, RenderPosition.BEFORE_END);
 
-    let showMovieStart = MOVIE_SHOW_START;
+    let showMovieCount = MOVIE_SHOW_START;
     const movieListContainerElement = this._movieContainerComponent.getElement();
 
-    movies.slice(0, showMovieStart).forEach((movie) => {
-      renderMovie(movieListContainerElement, movie);
-    });
+    renderMovies(movieListContainerElement, movies.slice(0, showMovieCount));
+    renderShowMoreButton();
 
-    render(movieListElement, this._showMoreButtonComponent, RenderPosition.BEFORE_END);
+    this._sortComponent.setSortTypeChangeHandler((sortType) => {
+      showMovieCount = MOVIE_SHOW_START;
 
-    this._showMoreButtonComponent.setClickHandler(() => {
-      const prevMovieShowCount = showMovieStart;
-      showMovieStart = showMovieStart + SHOWING_MOVIE_COUNT_BY_BUTTON;
+      const sortedMovies = getSortedMovies(sortType, movies, 0, showMovieCount);
 
-      movies.slice(prevMovieShowCount, showMovieStart).forEach((movie) => {
-        renderMovie(movieListContainerElement, movie);
-      });
+      movieListContainerElement.innerHTML = ``;
 
-      if (showMovieStart >= movies.length) {
-        remove(this._showMoreButtonComponent);
-      }
+      renderMovies(movieListContainerElement, sortedMovies);
+      renderShowMoreButton();
     });
 
     const moviesTopRated = getTopRatingMovies(movies.slice());
     const moviesMostCommented = getMostCommentedFilms(movies.slice());
 
+
     if (moviesTopRated[0].rating !== 0) {
-      renderExtraMovie(container, moviesTopRated, ExtraMovieTitle.TOP_RATED);
+      renderExtraMovie(filmsElement, moviesTopRated, ExtraMovieTitle.TOP_RATED);
     }
 
     if (moviesMostCommented[0].comments.length !== 0) {
-      renderExtraMovie(container, moviesMostCommented, ExtraMovieTitle.MOST_COMMENTED);
+      renderExtraMovie(filmsElement, moviesMostCommented, ExtraMovieTitle.MOST_COMMENTED);
     }
   }
 }
